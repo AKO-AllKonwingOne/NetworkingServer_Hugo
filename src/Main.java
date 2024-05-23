0343eeb035cdc58b7c0652b6e3210e43fe8dba8b
AKO-AllKonwingOne <hugo.lindgren.svensson@gmail.com>
2024-05-23 10:35:47 +0200

    klient och server

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ServerSocket server = null;
+        Socket client;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Default port number we are going to use
+        int portNumber = 50000;
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
         }
+        // Create Server side socket
+        try {
+            server = new ServerSocket(portNumber);
+        }catch (IOException ie) {
+            System.out.println("Cannot open port: " + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created" + server);
+
+        //Wait for data from client to reply
+        while (true) {
+            try {
+                //Listens for a connection to be made to this socket
+                // and accepts it. The method blocks until a connection is made
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request accepted...");
+                String clientHost = client.getInetAddress().getHostAddress();
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + clientHost + "Client port" + clientPort);
+
+                // Read data from client
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message received from client = " + msgFromClient);
+
+                //Send response to Client
+                if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")){
+                    OutputStream clientOut = client.getOutputStream();
+                    PrintWriter pw = new PrintWriter(clientOut, true);
+                    String ansMsg = "Hello " + msgFromClient;
+                    pw.println(ansMsg);
+                }
+
+                //Close sockets
+                if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")){
+                    server.close();
+                    client.close();
+
+                    break;
+                }
+
+            } catch (IOException ie){
+                //Error msg
+                System.out.println("Cannot accept connection: " + ie);
+            }
+        }
+
+
     }
 }
